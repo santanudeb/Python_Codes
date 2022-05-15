134e63f561769ebe2b59ada9bd93c703f035c27a
Santanu Deb <santanudeb1@gmail.com>
2022-05-15 09:13:46 +0530

    Add files via upload

---
+++ Madlibs.py
@@ -0,0 +1,10 @@
+''' silly madlibs'''
+
+adjective = input("pick a silly adjective : ")
+verb1 = input("pick a verb you love : ")
+verb2 = input("pick a another verb you hate: ")
+famous_person = input("pick a famous person : ")
+
+sentence = "Coding is " + adjective + "!!! " + "I love " + verb1 + " because I hate " + verb2 + " someday I want to be like " + famous_person
+
+print(sentence)
