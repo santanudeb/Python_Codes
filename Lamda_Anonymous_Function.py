8fd6ab3240b3009c4ce05042d04429c89e054d1c
Santanu Deb <santanudeb1@gmail.com>
2022-05-24 09:04:22 +0530

    Add files via upload

---
+++ Lamda_Anonymous_Function.py
@@ -0,0 +1,13 @@
+# Lamda or Anonymous function
+
+# function doing similar to lambda a, b : a + b
+def sum(n1,n2) :
+    return n1 + n2
+
+lam_sum = lambda a, b : a + b 
+
+val1 = sum(3, 9)
+val2 = lam_sum(3, 9)
+
+print(val1)
+print(val2)
